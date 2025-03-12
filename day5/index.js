8ebcb0453618d52976a5b1969ec8398e0402b35f
An Tran <anth24@fpt.com>
2025-03-12 00:36:53 -0400

    Update

---
+++ day5/index.js
@@ -0,0 +1,6 @@
+console.log("hello");
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+//for loop: continue, break keyword
+//forEach: continue by return, no break
+//forEach is simpler to use, but lack precise control
---
+++ day6/index.js
@@ -0,0 +1,16 @@
+//ways to write function in JS
+
+//1. Function declaration (common)
+function foo() {
+    
+}
+
+//2. 2nd way (less common)
+const bar = function (params) {
+    
+}
+
+//3. Arrow function (common)
+const baz = (a,b) => {
+    
+}
